aeb8529b405c3865433cc28946c294143d5b05d6
MgCohen <44907436+MgCohen@users.noreply.github.com>
2024-07-13 11:56:37 -0300

    added polarys and sample project

---
+++ Assets/Script/Directions/EnemyManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField] private GameManager gameManager;
+    [SerializeField] private Enemy enemyPrefab;
+    public List<Enemy> Enemies = new List<Enemy>();
+
+    private void Start()
+    {
+        Enemies = GetComponentsInChildren<Enemy>().ToList();
+        foreach (Enemy enemy in Enemies)
+        {
+            enemy.Setup(gameManager.Player);
+        }
+    }
+
+    public Enemy SpawnEnemy()
+    {
+        Enemy enemy = Instantiate(enemyPrefab);
+        enemy.Setup(gameManager.Player);
+        Enemies.Add(enemy);
+        return enemy;
+    }
+
+    public void DestroyEnemy(Enemy enemy)
+    {
+        Destroy(enemy.gameObject);
+        Enemies.Remove(enemy);
+    }
+}
+
---
+++ Assets/Script/Directions/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private GameManager gameManager;
+
+    [SerializeField] private float sensorRange = 5f;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private LayerMask mask;
+
+    private Vector3[] directions = new Vector3[4];
+    private Vector3 finalDir;
+
+
+    private void Update()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, sensorRange, mask);
+
+        directions = new Vector3[colliders.Length];
+        finalDir = Vector3.zero;
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            var direction =   colliders[i].transform.position - transform.position;
+            var totalDirection = direction.normalized * sensorRange;
+            directions[i] = -totalDirection + direction;
+            finalDir += directions[i];
+        }
+
+        transform.position += finalDir.normalized * Time.deltaTime * moveSpeed;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, sensorRange);
+        foreach(var direction in directions)
+        {
+            finalDir += direction;
+            Gizmos.DrawLine(transform.position, transform.position + direction);
+        }
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + finalDir);
+    }
+}
+
