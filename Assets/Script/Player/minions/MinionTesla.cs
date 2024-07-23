337897be1d9b9b3d3d116e736fd076b68e9d5356
Miguel  Tavares <miguelmst13@outlook.com>
2024-07-23 14:57:47 -0300

    zzz

--- Assets/Script/Player/minions/MinionTesla.cs
+++ Assets/Script/Player/minions/MinionTesla.cs
@@ -1,35 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MinionTesla : MonoBehaviour
 {
-    [SerializeField] private int teslaDamage;
-    [SerializeField] private float sensorRange = 5f;
     [SerializeField] private LayerMask mask;
-
-    private Vector3[] directions = new Vector3[4];
-    private Vector3 finalDir;
-
-    private EnemyHealth enemy;
-
+    [SerializeField] private float sensorRange = 5f;
+    [SerializeField] private int teslaDamage;
     [SerializeField] private Transform body;
 
-    private void Start()
-    {
-        enemy = GetComponent<EnemyHealth>();
-    }
+    [SerializeField] float fireRate = 1f;
+    public float fireCountDown;
+
+    [SerializeField] private Transform firePoint;
+
+    [SerializeField] private Player player;
+
     private void Update()
     {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(body.position, sensorRange, mask);
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, sensorRange, mask);
 
         Collider2D closest = null;
         float minDistance = 999;
 
         for (int i = 0; i < colliders.Length; i++)
         {
-            var distance = Vector3.Distance(colliders[i].transform.position, body.position);
+            var distance = Vector3.Distance(colliders[i].transform.position, transform.position);
             if (distance < minDistance)
             {
                 minDistance = distance;
@@ -39,22 +33,37 @@ public class MinionTesla : MonoBehaviour
         }
         if (closest != null)
         {
-            var enemyHealth = GetComponent<EnemyHealth>();
-            enemyHealth.TakeDamage(teslaDamage);
-            Debug.Log("danoInimigo");
+            var direction = closest.transform.position - transform.position;
+
+            body.up = direction;
 
         }
+
+        fireCountDown -= Time.deltaTime;
     }
+
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, sensorRange);
-        foreach (var direction in directions)
-        {
-            finalDir += direction;
-            Gizmos.DrawLine(transform.position, transform.position + direction);
-        }
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, transform.position + finalDir);
     }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (fireCountDown <= 0f)
+        {
+            if (collision.tag == "Inimigo")
+            {
+                var enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+                enemyHealth.TakeDamage(teslaDamage);
+                Debug.Log("danoTesla");
+
+            }
+        }
+        
+    }
+
 }
+
+
+
+
